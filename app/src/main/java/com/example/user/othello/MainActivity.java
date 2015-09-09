3d1eb63ac11e040700968b46e39bbb841138216b
nogami <nogami@a.b>
2015-09-09 22:02:09 +0900

    implement starting option

--- app/src/main/java/com/example/user/othello/MainActivity.java
+++ app/src/main/java/com/example/user/othello/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.user.othello;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -11,6 +14,16 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView title = (TextView)findViewById(R.id.topTitle);
+        title.setOnClickListener(new ButtonOnClickListener());
+    }
+
+    class ButtonOnClickListener implements View.OnClickListener {
+        public void onClick(View v){
+            Intent intent = new Intent(MainActivity.this, StartingOption.class);
+            startActivity(intent);
+        }
     }
 
     @Override
